e0e204d276baccb0f8b6478b85ae3288b157b05d
Caitlin Cahill <caitlin.cahill@gmail.com>
2015-12-05 13:19:46 -0600

    career search form

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,14 +1,19 @@
+var app = document.querySelector('#app');
+
+app.user = {
+	firstName: 'Kyra',
+	lastName:'Bhandari',
+	pic: 'images/avatar.jpg',
+	email: 'kbhand007@school.org',
+	id:0,
+	firstRun:true
+}
+
+var firstRun = true;
+
 (function(document) {
   'use strict';
 
-	var app = document.querySelector('#app');
-
-	app.user = {
-		firstName: 'Kyra',
-		lastName:'Bhandari',
-		pic: 'images/avatar.jpg',
-		email: 'kbhand007@school.org',
-		id:0
-	}
+	
 
 })(document);
